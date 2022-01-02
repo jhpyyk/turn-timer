2cbc94b0495303ac11498e1fa7dd1299049f2c8b
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2022-01-02 23:42:40 +0200

    Delete PlayerView.js

--- PlayerView.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Pressable, Text } from "react-native";
-import Player from "./Player";
-import Timer from "./Timer";
-
-export default function PlayerView() {
-  return;
-}
