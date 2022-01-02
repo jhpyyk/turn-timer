3ddf818da5fd17e90a7ac98810dc273c7a6707ba
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2022-01-02 23:06:21 +0200

    Add player box

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Timer from './Timer.js';
-import EndTurnButton from './EndTurnButton.js';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Timer from "./Timer.js";
+import EndTurnButton from "./EndTurnButton.js";
+import Player from "./Player.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Timer duration = {10 * 60 * 1000} />
+      <Player duration={10 * 60 * 1000} />
+      <Timer duration={10 * 60 * 1000} />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,9 +17,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 50,
   },
 });
--- Player.js
+++ Player.js
@@ -1,6 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Pressable, Text } from "react-native";
+import TimeDisplay from "./TimeDisplay";
 
 export default function Player(props) {
-  return;
+  const [timeLeft, setTimeLeft] = useState(props.duration);
+  return (
+    <Pressable style={styles.boxStyle}>
+      <TimeDisplay duration={timeLeft} />
+    </Pressable>
+  );
 }
+
+const styles = StyleSheet.create({
+  boxStyle: {
+    width: 150,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 20,
+    borderRadius: 10,
+    backgroundColor: "orange",
+  },
+});
