cea216df55a4f4273312af1a6552d26482996945
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-02-04 19:54:58 +0200

    Navigation bar hides after a short time

---
+++ AppStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "#1C2833",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default styles;
