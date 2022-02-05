6b3118a7ddfa80d95970ac0b0596f3660b842d25
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-02-05 21:09:50 +0200

    Navigation bar visibility uses new API

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import AddPlayersForm from "./src/screens/AddPlayersForm/AddPlayersForm.js";
@@ -7,6 +8,7 @@ import styles from "./AppStyles.js";
 
 export default function App() {
   const [screen, setScreen] = useState(null);
+  const [navBarVisibility, setNavBarVisibility] = useState("visible");
 
   const addPlayersDoneHandle = (info) => {
     if (info.length > 0) {
@@ -15,6 +17,25 @@ export default function App() {
   };
 
   useEffect(() => {
+    if (navBarVisibility === "visible") {
+      const interval = setTimeout(() => {
+        NavigationBar.setVisibilityAsync("hidden");
+      }, /* 3 Seconds */ 3000);
+
+      return () => {
+        clearTimeout(interval);
+      };
+    }
+  }, [navBarVisibility]);
+
+  useEffect(() => {
+    NavigationBar.addVisibilityListener(({ visibility }) => {
+      setNavBarVisibility(visibility);
+    });
+    NavigationBar.setPositionAsync("absolute");
+    NavigationBar.setVisibilityAsync("hidden");
+    NavigationBar.setBehaviorAsync("inset-swipe");
+    NavigationBar.setBackgroundColorAsync("#00000080");
     setScreen(<AddPlayersForm addPlayersDoneHandle={addPlayersDoneHandle} />);
   }, []);
 
