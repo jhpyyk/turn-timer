93a8d91d0e5a85872977904c4d2ebfaf9b8c1688
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-02-05 18:28:40 +0200

    Fix bug in getTime in UseTimer

--- src/hooks/UseTimer.js
+++ src/hooks/UseTimer.js
@@ -15,7 +15,7 @@ export default function useTimer(time, playerIndex) {
   };
 
   const getTime = () => {
-    return calculateTimeLeft();
+    return calculateTimeLeft(Date.now());
   };
 
   const calculateTimeLeft = (timeStamp) => {
